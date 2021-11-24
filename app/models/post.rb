2818b68bfce0918e4e42d1228ff01949b53bfbd2
Gregory Olive <gregoryolive@outlook.com>
2021-11-24 12:36:59 +0900

    Add user to post model and fix views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   CLUBS = %w[Art Books Business Food Gaming History Jokes Movies Music Nature Sports Tech Television].freeze
 
-  validates :title, presence: true, length: { in: 6..40 }
+  validates :title, presence: true, length: { in: 5..40 }
   validates :body, presence: true, length: { minimum: 5 }
   validates :club, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_163510) do
-
-  create_table "clubs", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_11_22_133603) do
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
@@ -24,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_21_163510) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "club"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
