cd7046c003444d256ece2888a3d9ca3daa583377
Gregory Olive <gregoryolive@outlook.com>
2021-11-28 01:23:33 +0900

    Fix views when no posts, fix missing dependent in post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { in: 5..40 }
