b91d5348f84a4c7a035c4fc79071a9428f146124
Gregory Olive <gregoryolive@outlook.com>
2021-11-28 02:40:17 +0900

    Delete migration file for Heroku

--- db/migrate/20211121053943_remove_columns.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumns < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :posts, :club_id
-  end
-end
--- db/migrate/20211125045649_drop_clubs_table.rb
+++
@@ -1,9 +0,0 @@
-class DropClubsTable < ActiveRecord::Migration[6.1]
-  def up
-    drop_table :clubs
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
